2e29919d713f0d42dd166d1b0f783e11365dd7b9
UnfussyRogue8 <kieco54@icloud.com>
2025-11-02 17:41:27 -0800

    fixed issue with last commit: line 11 of main.py forgot to make player a class object

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         # The Game Loop
