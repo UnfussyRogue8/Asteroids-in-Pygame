33eff1c19b6cfd9f2a74a0ba5f3686d09197ec6a
UnfussyRogue8 <kieco54@icloud.com>
2025-11-02 17:10:07 -0800

    added circleshape.py for object classes. Was just copied and pasted

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
+from constants import *
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         # The Game Loop
@@ -13,6 +16,9 @@ def main():
 
         screen.fill(color="black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
